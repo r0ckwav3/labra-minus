99d5cb379648805f528524749b8ac9a686a3b381
r0ckwav3 <peterrockwave@gmail.com>
2023-04-10 12:37:43 -0700

    made (failing) test for add and concat

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,42 @@ fn main() {
         Err(e) => {println!("Error: {:?}", e);}
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn addition_test() {
+        let expr = "1(2)(6)";
+        let pt = parsetree::parse(expr).expect("parse error");
+        let result = evaluate::evaluate(&pt, &value::Value::Number(0)).expect("evaluation failure");
+        if let value::Value::Number(n) = result{
+            assert_eq!(n, 9);
+        }else{
+            panic!("Bad return type");
+        }
+    }
+
+    #[test]
+    fn concat_test() {
+        let expr = "2[](3[])([][])";
+        // should return [2,3,[]]
+        let pt = parsetree::parse(expr).expect("parse error");
+        let result = evaluate::evaluate(&pt, &value::Value::Number(0)).expect("evaluation failure");
+        if let value::Value::List(l) = result{
+            let len = l.length().expect("indexing failure");
+            assert_eq!(len, 3);
+            match l.index(0).expect("indexing failure"){
+                value::Value::Number(n) => assert_eq!(n, 2),
+                _ => panic!("bad return type")
+            }
+            match l.index(1).expect("indexing failure"){
+                value::Value::Number(n) => assert_eq!(n, 2),
+                _ => panic!("bad return type")
+            }
+        }else{
+            panic!("Bad return type");
+        }
+    }
+}
