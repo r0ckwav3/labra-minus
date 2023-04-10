464359f1d1ce664b09c4e35420aa848c8182210a
r0ckwav3 <peterrockwave@gmail.com>
2023-04-10 10:12:52 -0700

    started on evaluate

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,6 +1,34 @@
+use std::rc::Rc;
+
+use super::value;
 use super::value::Value;
 use super::parsetree::ParseTree;
 
-pub fn evaluate(expression: &ParseTree, input: &Value) -> Value{
-    Value::Number(0)
+pub fn evaluate(expression: &ParseTree, input: &Value) -> Result<Value, value::RuntimeError>{
+    match expression {
+        ParseTree::Number(n) => Ok(Value::Number(n.clone())),
+
+        ParseTree::Input => Ok(*input),
+
+        ParseTree::EmptyList => Ok(Value::List(Rc::new(value::ExactList::new(Vec::new())))),
+
+        ParseTree::Length(pt) => match evaluate(pt, input)? {
+            Value::Number(n) => Ok(Value::Number(n.abs())),
+            Value::List(l) => Ok(Value::Number(l.length()?)),
+        },
+
+        ParseTree::Encapsulate(pt) => {
+            let mut newlist = Vec::new();
+            newlist.push(evaluate(pt, input)?);
+            Ok(Value::List(Rc::new(value::ExactList::new(newlist))))
+        },
+
+        ParseTree::Addition(pt1, pt2) => Ok(Value::Number(0)),
+
+        ParseTree::IndexSubtraction(pt1, pt2) => Ok(Value::Number(0)),
+
+        ParseTree::Induction(pt1, pt2) => Ok(Value::Number(0)),
+
+        ParseTree::Map(pt1, pt2) => Ok(Value::Number(0))
+    }
 }
--- src/value.rs
+++ src/value.rs
@@ -8,69 +8,96 @@ pub enum Value {
     List(Rc<dyn ListLike>),
 }
 
-pub enum ListError {
+pub enum RuntimeError {
     OutOfBounds,
     ResolvingInfiniteList
 }
 
 pub trait ListLike {
-    fn index(&self, i: usize) -> Result<Value, ListError>;
-    fn length(&self) -> Result<Value, ListError>;
+    fn index(&self, i: usize) -> Result<Value, RuntimeError>;
+    fn length(&self) -> Result<i64, RuntimeError>;
 }
 
-struct ExactList{
+pub struct ExactList{
     contents: Vec<Value>
 }
 
-struct LazyInductionList {
+pub struct LazyInductionList {
     function: ParseTree,
     initial_value: Value,
     resolved: RefCell<Vec<Value>>,
 }
 
-struct LazyMapList {
+pub struct LazyMapList {
     function: ParseTree,
     source: Box<dyn ListLike>
 }
 
+impl ExactList{
+    pub fn new(c: Vec<Value>) -> ExactList{
+        ExactList{
+            contents: c
+        }
+    }
+}
+
 impl ListLike for ExactList{
-    fn index(&self, i: usize) -> Result<Value, ListError>{
+    fn index(&self, i: usize) -> Result<Value, RuntimeError>{
         if i >= self.contents.len(){
-            Err(ListError::OutOfBounds)
+            Err(RuntimeError::OutOfBounds)
         }else{
             Ok(self.contents[i].clone())
         }
     }
 
-    fn length(&self) -> Result<Value, ListError>{
-        return Ok(Value::Number(self.contents.len() as i64));
+    fn length(&self) -> Result<i64, RuntimeError>{
+        return Ok(self.contents.len() as i64);
+    }
+}
+
+impl LazyInductionList{
+    pub fn new(f: ParseTree, init: Value) -> LazyInductionList{
+        LazyInductionList{
+            function: f,
+            initial_value: init,
+            resolved: RefCell::new(Vec::new())
+        }
     }
 }
 
 impl ListLike for LazyInductionList {
-    fn index(&self, i: usize) -> Result<Value, ListError>{
+    fn index(&self, i: usize) -> Result<Value, RuntimeError>{
         let mut resolved = self.resolved.borrow_mut();
         if resolved.len() == 0 {
-            resolved.push(evaluate::evaluate(&self.function, &self.initial_value));
+            resolved.push(evaluate::evaluate(&self.function, &self.initial_value)?);
         }
         while i > resolved.len() {
             let prevresolved = resolved[resolved.len()-1].clone();
-            resolved.push(evaluate::evaluate(&self.function, &prevresolved));
+            resolved.push(evaluate::evaluate(&self.function, &prevresolved)?);
         }
         Ok(resolved[i].clone())
     }
 
-    fn length(&self) -> Result<Value, ListError>{
-        return Err(ListError::ResolvingInfiniteList);
+    fn length(&self) -> Result<i64, RuntimeError>{
+        return Err(RuntimeError::ResolvingInfiniteList);
+    }
+}
+
+impl LazyMapList{
+    pub fn new(f: ParseTree, s: Box<dyn ListLike>) -> LazyMapList{
+        LazyMapList{
+            function: f,
+            source: s
+        }
     }
 }
 
 impl ListLike for LazyMapList {
-    fn index(&self, i: usize) -> Result<Value, ListError>{
+    fn index(&self, i: usize) -> Result<Value, RuntimeError>{
         self.source.index(i).map(|v| evaluate::evaluate(&self.function, &v))
     }
 
-    fn length(&self) -> Result<Value, ListError>{
+    fn length(&self) -> Result<i64, RuntimeError>{
         return self.source.length();
     }
 }
